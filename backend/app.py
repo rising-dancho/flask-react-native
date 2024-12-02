245ca5c942cf1a3230bdafac4948023865f3d8e9
adfinem <zenlaws02@gmail.com>
2024-12-02 19:18:48 +0800

    feat: setup a connection to the mysql db

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,25 @@
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:@localhost:3306/flask"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+
+db = SQLAlchemy(app)
+
+
+class Articles(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    body = db.Column(db.Text())
+    date = db.Column(db.DateTime, default=datetime.datetime.now)
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
 
 
 @app.route("/", methods=["GET"])
@@ -9,4 +28,6 @@ def get_articles():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    with app.app_context():
+        db.create_all()
+    print("Database tables created successfully.")
