62ecd92935ae0360fe5d6598938f896a29cde981
adfinem <zenlaws02@gmail.com>
2024-12-03 20:27:05 +0800

    feat: in progress, working on the index page

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Welcome to python, flask, and react-native</Text>
     </View>
   );
 }
