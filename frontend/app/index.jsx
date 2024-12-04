8fa09b795a22f8a3a2497ebe6b33df041c558387
adfinem <zenlaws02@gmail.com>
2024-12-04 17:01:34 +0800

    feat: added a flatlist component

--- frontend/app/index.jsx
+++ frontend/app/index.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import { View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import { PaperProvider } from 'react-native-paper';
 import Home from '../components/Home.jsx';
 
 export default function Index() {
   const name = 'adfinem';
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Home name={name} />
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Home name={name} />
+        <StatusBar style="auto" />
+      </View>
+    </PaperProvider>
   );
 }
--- frontend/components/Home.jsx
+++ frontend/components/Home.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, Flatlist } from 'react-native';
 
 function Home({ name }) {
+  const data = [
+    {
+      id: 1,
+      title: 'First title',
+      body: 'First body',
+    },
+    {
+      id: 2,
+      title: 'Second title',
+      body: 'Second body',
+    },
+    {
+      id: 3,
+      title: 'Third title',
+      body: 'Third body',
+    },
+  ];
+
   return (
     <View>
+      <Flatlist
+        data={data}
+        renderItem={(data) => {
+          console.log(data);
+        }}
+        keyExtractor={(item) => `${item.id}`}
+      />
       <Text>Welcome to Python, Flask, and React Native</Text>
       <Text>This is my second text</Text>
       <Text>{name}</Text>
