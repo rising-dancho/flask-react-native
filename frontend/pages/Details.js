a66d2a4eedc21f12795680f2ff5abf371cc72fd3
adfinem <zenlaws02@gmail.com>
2024-12-08 15:36:25 +0800

    feat: in progress, updated the ui design of the Details page

--- frontend/pages/Details.js
+++ frontend/pages/Details.js
@@ -1,16 +1,40 @@
 import React from 'react';
-import { View, ScrollView, Text } from 'react-native';
+import { ScrollView, Text, StyleSheet } from 'react-native';
+import { Card } from 'react-native-paper';
 
 function Details({ route }) {
   const data = route.params.data;
   return (
     <ScrollView>
-      <View>
-        <Text>{data.title}</Text>
-        <Text>{data.body}</Text>
-      </View>
+      <Card style={styles.cardStyle}>
+        <Text style={styles.title}>{data.title}</Text>
+        <Text style={styles.body}>{data.body}</Text>
+        <Text style={styles.date}>{data.date}</Text>
+      </Card>
     </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  cardStyle: {
+    padding: 10,
+    margin: 10,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: '700',
+    marginBottom: 6,
+  },
+  body: {
+    fontSize: 20,
+    marginBottom: 3,
+  },
+  date: {
+    fontSize: 12,
+  },
+});
 
 export default Details;
